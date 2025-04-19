61520c597717a4c54b57f5d6cb3a6173d2781807
Daniil Loskutov <donakass567@gmail.com>
2025-04-19 15:01:40 +1000

    commit
    fsdfasdf

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitButton : MonoBehaviour
+{
+    public string mainMenuSceneName = "MainMenu"; // Имя сцены главного меню
+
+    public void OnExitButtonClick()
+    {
+        // Загружаем сцену главного меню
+        SceneManager.LoadScene(mainMenuSceneName);
+
+        // Возобновляем время
+        Time.timeScale = 1f;
+    }
+}
